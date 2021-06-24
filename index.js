444ec4687d525948311fe9058c08d926036d7623
NewExample <timurmanilov12@mail.ru>
2021-06-24 03:48:16 +0300

    2-nd

--- index.js
+++ index.js
@@ -1,45 +1,123 @@
-  const score = document.querySelector('.score'),
-    start = document.querySelector('.start'),
-    gameArea = document.querySelector('.gameArea'),
-    car = document.createElement('div');
+const score = document.querySelector('.score'),
+  start = document.querySelector('.start'),
+  gameArea = document.querySelector('.gameArea'),
+  car = document.createElement('div');
 
-  start.addEventListener('click', startGame);
-  document.addEventListener('keydown', startRun);
-  document.addEventListener('keyup', stopRun);
+car.classList.add('car');
 
-  const keys = {
-    ArrowUp: false,
-    ArrowDown: false,
-    ArrowRight: false,
-    ArrowLeft: false
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+const keys = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowRight: false,
+  ArrowLeft: false
+};
+
+const setting = {
+  start: false,
+  score: 0,
+  speed: 7,
+  // чем меньше значение трафика тем сложнее.меньше растояние
+  traffic: 3
+};
+
+function getQuantityElements(heightElement) {
+  return document.documentElement.clientHeight / heightElement + 1;
+}
+
+function startGame() {
+  start.classList.add('hide');
+  // добавление линий
+  for (let i = 0; i < getQuantityElements(100); i++) {
+    const line = document.createElement('div');
+    line.classList.add('line');
+    line.style.top = (i * 100) + 'px';
+    line.y = i * 100;
+    gameArea.appendChild(line);
   }
 
-  const setting = {
-    start: false,
-    score: 0,
-    speed: 3
+  // добавление машин
+  for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
+    const enemy = document.createElement('div');
+    enemy.classList.add('enemy');
+    enemy.y = -100 * setting.traffic * (i + 1);
+    enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    enemy.style.top = enemy.y + 'px';
+    gameArea.appendChild(enemy);
   }
 
-  function startGame(){
-    start.classList.add('hide');
-    setting.start = true;
-    gameArea.appendChild(car); // car будет дочерним элементом в div
-    car.classList.add('car')
+  setting.start = true;
+  gameArea.appendChild(car);
+  setting.x = car.offsetLeft;
+  setting.y = car.offsetTop;
+  requestAnimationFrame(playGame);
+}
+
+function playGame() {
+  if (setting.start) {
+    moveRoad();
+    moveEnemy();
+    if (keys.ArrowLeft && setting.x > 0) {
+      setting.x -= setting.speed;
+    }
+    if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
+      setting.x += setting.speed;
+    }
+
+    if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
+      setting.y += setting.speed;
+    }
+
+    if (keys.ArrowUp && setting.y > 0) {
+      setting.y -= setting.speed;
+    }
+
+    car.style.left = setting.x + 'px';
+    car.style.top = setting.y + 'px';
+
     requestAnimationFrame(playGame);
   }
 
-  function playGame(){
-    if (setting.start) {
-      requestAnimationFrame(playGame);
+}
+
+function startRun(event) {
+  event.preventDefault();
+  keys[event.key] = true;
+}
+
+function stopRun(event) {
+  event.preventDefault();
+  keys[event.key] = false;
+}
+
+// движение линий
+function moveRoad() {
+  let lines = document.querySelectorAll('.line');
+  lines.forEach(function (line) {
+    line.y += setting.speed;
+    line.style.top = line.y + 'px';
+
+    if (line.y >= document.documentElement.clientHeight) {
+      line.y = -100;
     }
-  }
 
-  function startRun(event){
-    event.preventDefault(); // Отменяет прокрутку страницы
-    keys[event.key] = true;
-  }
+  });
+}
 
-  function stopRun(event){
-    event.preventDefault(); // Отменяет прокрутку страницы
-    keys[event.key] = false;
-  }
+// движение машин
+function moveEnemy() {
+  let enemy = document.querySelectorAll('.enemy');
+  enemy.forEach(function (item) {
+    item.y += setting.speed / 2;
+    item.style.top = item.y + 'px';
+
+    if (item.y >= document.documentElement.clientHeight) {
+      item.y = -100 * setting.traffic;
+      item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    }
+
+  });
+}
